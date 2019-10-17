beb2115ed55317048e95036a8cd0efe000f9e6f2
nikolay <83adnike@gmail.com>
2019-10-17 22:35:03 +0300

    Fibonacci-function print-grocery

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,11 @@
 parents, babies = (1, 1)
 while babies < 100:
-    print ('This generation has {0} babies'.format(babies))
+    print('This generation has {0} babies'.format(babies))
     parents, babies = (babies, parents + babies)
 
+
 def greet(name):
-    print ('Hello', name)
+    print('Hello', name)
 
 greet('Jack')
 greet('Jill')
@@ -16,4 +17,4 @@ my_purchase = {
     'banana': 6}
 grocery_bill = sum(prices[fruit] * my_purchase[fruit]
                    for fruit in my_purchase)
-print ('I owe the grocer $%.2f' % grocery_bill)
+print('I owe the grocer $%.2f' % grocery_bill)
