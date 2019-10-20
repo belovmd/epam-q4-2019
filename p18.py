cd9c3a94b3a64cfee099d066453aad758219dbdd
par216 <56726139+par216@users.noreply.github.com>
2019-10-20 19:20:48 +0300

    Update p18.py

--- p18.py
+++ p18.py
@@ -1,5 +1,6 @@
 BOARD_SIZE = 8
 
+
 def under_attack(col, queens):
     left = right = col
 
@@ -10,15 +11,25 @@ def under_attack(col, queens):
             return True
     return False
 
+
 def solve(n):
     if n == 0:
         return [[]]
 
     smaller_solutions = solve(n - 1)
 
-    return [solution+[(n,i+1)]
+    return [solution+[(n, i+1)]
         for i in range(BOARD_SIZE)
             for solution in smaller_solutions
                 if not under_attack(i+1, solution)]
 for answer in solve(BOARD_SIZE):
     print (answer)
+
+# read stocks data, print status messages
+with open('stocks.csv', 'r') as stocksFile:
+    stocks = csv.reader(stocksFile)
+
+    status_labels = {-1: 'down', 0: 'unchanged', 1: 'up'}
+    for ticker, name, price, change, pct in stocks:
+        status = status_labels[cmp(float(change), 0.0)]
+        print ('%s is %s (%.2f)' % (name, status, float(pct)))
