7e7eb33c4a2565fe368ddd8b7173822308f20879
windsdel <51128452+windsdel@users.noreply.github.com>
2019-10-20 21:17:27 +0300

    Update HW1_18.py

--- HW1_18.py
+++ HW1_18.py
@@ -7,9 +7,9 @@ def iter_primes():
 
 # generate primes forever
     while True:
-    # get the first number from the iterator (always a prime)
-    prime = next(numbers)
-        yield prime
+        # get the first number from the iterator (always a prime)
+        prime = next(numbers)
+yield prime
 
 # this code iteratively builds up a chain of
 # filters...slightly tricky, but ponder it a bit
@@ -17,5 +17,5 @@ def iter_primes():
 
 for p in iter_primes():
     if p > 1000:
-    break
+        break
 print(p)
