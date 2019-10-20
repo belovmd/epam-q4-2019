724d909d5c93ef7c08ce451988f3b6f0e42713c7
windsdel <51128452+windsdel@users.noreply.github.com>
2019-10-20 16:39:34 +0300

    Update HW1_19.py

--- HW1_19.py
+++ HW1_19.py
@@ -1,3 +1,7 @@
+# From http://effbot.org/zone/element-index.htm
+import xml.etree.ElementTree as etree
+tree = etree.fromstring(dinner_recipe)
+
 dinner_recipe = '''<html><body><table>
 <tr><th>amt</th><th>unit</th><th>item</th></tr>
 <tr><td>24</td><td>slices</td><td>baguette</td></tr>
@@ -5,10 +9,6 @@ dinner_recipe = '''<html><body><table>
 <tr><td>1</td><td>cup</td><td>tomatoes</td></tr>
 <tr><td>1</td><td>jar</td><td>pesto</td></tr>
 </table></body></html>'''
-
-# From http://effbot.org/zone/element-index.htm
-import xml.etree.ElementTree as etree
-tree = etree.fromstring(dinner_recipe)
 
 # For invalid HTML use http://effbot.org/zone/element-soup.htm
 # import ElementSoup, StringIO
@@ -18,4 +18,4 @@ pantry = set(['olive oil', 'pesto'])
 for ingredient in tree.getiterator('tr'):
     amt, unit, item = ingredient
     if item.tag == "td" and item.text not in pantry:
-        print ("%s: %s %s" % (item.text, amt.text, unit.text))
+        print("%s: %s %s" % (item.text, amt.text, unit.text))
