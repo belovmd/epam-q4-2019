dab551afbf397533ac5a89891a2ec7c87bdf5f2c
windsdel <51128452+windsdel@users.noreply.github.com>
2019-10-20 16:37:16 +0300

    Update HW1_14.py

--- HW1_14.py
+++ HW1_14.py
@@ -1,6 +1,6 @@
 def median(pool):
     '''Statistical median to demonstrate doctest.
-    >>> median([2, 9, 9, 7, 9, 2, 4, 5, 8])
+>>> median([2, 9, 9, 7, 9, 2, 4, 5, 8])
     6 #change to 7 in order to pass the test
     '''
     copy = sorted(pool)
@@ -9,6 +9,6 @@ def median(pool):
         return copy[int((size - 1) / 2)]
     else:
         return (copy[int(size/2 - 1)] + copy[int(size/2)]) / 2
-if __name__ == '__main__':
+    if __name__ == '__main__':
     import doctest
     doctest.testmod()
