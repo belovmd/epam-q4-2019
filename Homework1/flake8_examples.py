0e0616a90b6d0fc55346402c6891e7428046d907
JustgoodDeal <stostoraz@gmail.com>
2019-10-20 04:11:26 +0300

    reduced length of the line

--- Homework1/flake8_examples.py
+++ Homework1/flake8_examples.py
@@ -37,7 +37,7 @@ grocery_bill = sum(prices[fruit] * my_purchase[fruit]
                    for fruit in my_purchase)
 print('I owe the grocer $%.2f' % grocery_bill)
 
-# This program adds up integers that have been passed as arguments in the command line
+# adds up integers that have been passed as arguments in the command line
 try:
     total = sum(int(arg) for arg in sys.argv[1:])
     print('sum =', total)
