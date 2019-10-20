40c7c1989343238076036b05f099a62603d9fb25
windsdel <51128452+windsdel@users.noreply.github.com>
2019-10-20 21:08:20 +0300

    Update HW1_13.py

--- HW1_13.py
+++ HW1_13.py
@@ -3,7 +3,6 @@ import unittest
 
 def median(pool):
 
-
     copy = sorted(pool)
     size = len(copy)
 
@@ -11,14 +10,14 @@ def median(pool):
         return copy[int((size - 1) / 2)]
 
     else:
-        return (copy[int(size/2 - 1)] + copy[int(size/2)]) / 2
+        return (copy[int(size / 2 - 1)] + copy[int(size / 2)]) / 2
 
 
 class TestMedian(unittest.TestCase):
 
 
-def testMedian(self):
-    self.assertEqual(median([2, 9, 9, 7, 9, 2, 4, 5, 8]), 7)
+    def testMedian(self):
+        self.assertEqual(median([2, 9, 9, 7, 9, 2, 4, 5, 8]), 7)
 
 if __name__ == '__main__':
     unittest.main()
