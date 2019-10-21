52eee5d443222283117764729f7da0aa926eae29
par216 <56726139+par216@users.noreply.github.com>
2019-10-21 17:39:09 +0300

    Update p8.py

--- p8.py
+++ p8.py
@@ -3,6 +3,6 @@
 import sys
 try:
     total = sum(int(arg) for arg in sys.argv[1:])
-    print ('sum =', total)
+    print('sum =', total)
 except ValueError:
-    print ('Please supply integer arguments')
+    print('Please supply integer arguments')
