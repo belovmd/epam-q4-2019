42d42e2eea4485c2d5ec25f8cedde0f29f6712b5
par216 <56726139+par216@users.noreply.github.com>
2019-10-22 14:56:17 +0300

    Update p13.py

--- p13.py
+++ p13.py
@@ -8,12 +8,12 @@ def median(pool):
         return copy[int((size - 1) / 2)]
     else:
         return (copy[int(size / 2 - 1)] + copy[int(size / 2)]) / 2
-        
-        
+    
+    
 class TestMedian(unittest.TestCase):
     def testMedian(self):
         self.assertEqual(median([2, 9, 9, 7, 9, 2, 4, 5, 8]), 7)
-        
+
         
 if __name__ == '__main__':
     unittest.main()
