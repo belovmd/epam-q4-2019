3d63dc8a6ab0098f08a707a4014a372d9fd8facc
par216 <56726139+par216@users.noreply.github.com>
2019-10-22 15:02:28 +0300

    Update p14.py

--- p14.py
+++ p14.py
@@ -3,14 +3,15 @@ def median(pool):
     >>> median([2, 9, 9, 7, 9, 2, 4, 5, 8])
     6 #change to 7 in order to pass the test
     '''
+    
     copy = sorted(pool)
     size = len(copy)
     if size % 2 == 1:
         return copy[int((size - 1) / 2)]
     else:
         return (copy[int(size / 2 - 1)] + copy[int(size / 2)]) / 2
-    
-    
+
+
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
