d874eb7eb7d979310e0e8052a9183ff2a2f0dbb1
par216 <56726139+par216@users.noreply.github.com>
2019-10-22 15:03:53 +0300

    Update p13.py

--- p13.py
+++ p13.py
@@ -14,6 +14,6 @@ class TestMedian(unittest.TestCase):
     def testMedian(self):
         self.assertEqual(median([2, 9, 9, 7, 9, 2, 4, 5, 8]), 7)
 
-  
+
 if __name__ == '__main__':
     unittest.main()
