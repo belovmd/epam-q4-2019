c723db2648f805f0b08ec6c5abf188459a8c68b1
windsdel <51128452+windsdel@users.noreply.github.com>
2019-10-26 21:31:32 +0300

    Update HW1_14.py

--- HW1_14.py
+++ HW1_14.py
@@ -12,12 +12,12 @@ def median(pool):
 copy = sorted(pool)
 size = len(copy)
 
-if size % 2 == 1:
-    return copy[int((size - 1) / 2)]
-else:
-    return (copy[int(size / 2 - 1)] + copy[int(size / 2)]) / 2
+    if size % 2 == 1:
+        return copy[int((size - 1) / 2)]
+    else:
+        return (copy[int(size / 2 - 1)] + copy[int(size / 2)]) / 2
 
-if __name__ == '__main__':
-    import doctest
+    if __name__ == '__main__':
+        import doctest
 
 doctest.testmod()
