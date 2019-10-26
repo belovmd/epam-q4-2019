b15169801b638edcfafe6269c5382cfbd1fd346f
windsdel <51128452+windsdel@users.noreply.github.com>
2019-10-26 21:33:36 +0300

    Update HW1_14.py

--- HW1_14.py
+++ HW1_14.py
@@ -14,7 +14,7 @@ size = len(copy)
 
 if size % 2 == 1:
         return copy[int((size - 1) / 2)]
-    else:
+else:
         return (copy[int(size / 2 - 1)] + copy[int(size / 2)]) / 2
 
     if __name__ == '__main__':
