b38541effdef780b979edc9657865d53dbd8af1c
windsdel <51128452+windsdel@users.noreply.github.com>
2019-10-26 21:54:14 +0300

    Update HW1_17.py

--- HW1_17.py
+++ HW1_17.py
@@ -24,7 +24,7 @@ def solve(n):
 
            for i in range(BOARD_SIZE)
 
-           or solution in smaller_solutions
+        or solution in smaller_solutions
 
            if not under_attack(i + 1, solution)]
 
