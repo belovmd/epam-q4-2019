4aeafc5ae8afd17f0370ef4f34db97216f6ead45
Vadim Rashkevich <zawerdom@gmail.com>
2019-10-27 00:33:05 +0300

    Fixed

--- Homework2/MoneyCalculation.py
+++ Homework2/MoneyCalculation.py
@@ -3,4 +3,5 @@ if __name__ == '__main__':
     cents = int(input('Enter cents '))
     quantity = int(input('Enter quantity '))
     total_price = (dollars * 100 + cents) * quantity
-    print('Total price: {} dollars {} cents'.format(total_price // 100, total_price % 100))
+    print('Total price: {} dollars '
+          '{} cents'.format(total_price // 100, total_price % 100))
