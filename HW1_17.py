1aeafeb967918216545136e0902fbdf4e1f23778
windsdel <51128452+windsdel@users.noreply.github.com>
2019-10-26 21:44:00 +0300

    Update HW1_17.py

--- HW1_17.py
+++ HW1_17.py
@@ -1,10 +1,9 @@
 BOARD_SIZE = 8
 
 
-def under_attack(col, queens):
-    left = right = col
+def under_attack(col, queens): left = right = col
 
-for r, c in reversed(queens):
+for r, c in reversed(queens): 
         left, right = left - 1, right + 1
 
         if c in (left, col, right):
@@ -21,7 +20,6 @@ def solve(n):
     smaller_solutions = solve(n - 1)
 
     return[solution + [(n, i + 1)]
-
            for i in range(BOARD_SIZE)
            or solution in smaller_solutions
            if not under_attack(i + 1, solution)]
