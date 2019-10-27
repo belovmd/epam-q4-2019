78af2af0ccf4c0067685c6386eedcd8666f5df75
par216 <56726139+par216@users.noreply.github.com>
2019-10-27 18:56:59 +0300

    Update HW2-3.py

--- HW2-3.py
+++ HW2-3.py
@@ -1,16 +1,16 @@
 '''
-FizzBuzz 
+FizzBuzz
 '''
 
 for i in range(100):
 
     if i % 5 == 0 and i % 3 == 0:
-        i = "FizzBuzz" 
+        i = "FizzBuzz"
 
     elif i % 3 == 0:
-        i = "Fizz" 
+        i = "Fizz"
 
     elif i % 5 == 0:
         i = "Buzz"
-        
+ 
     print(i)
