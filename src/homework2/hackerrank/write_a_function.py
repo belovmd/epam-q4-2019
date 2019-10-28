74ed1d77bd0d606d2c67fc710958b68a7ae85ee8
Olga <o.f.akhmetova@gmail.com>
2019-10-28 17:14:28 +0300

    optimization
    delete unnecessary comparisons

--- src/homework2/hackerrank/write_a_function.py
+++ src/homework2/hackerrank/write_a_function.py
@@ -14,7 +14,6 @@ value (True/False)"""
 
 def is_leap(year):
     leap = False
-    if year % 4 == 0:
-        if year % 100 or year % 100 == 0 and year % 400 == 0:
-            leap = True
+    if year % 4 == 0 and year % 100 or year % 400 == 0:
+        leap = True
     return leap
