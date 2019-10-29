50eb8ea40f9e8c05eae7b616495654d62d5e4ab6
Olga <o.f.akhmetova@gmail.com>
2019-10-29 17:54:52 +0300

    implement correct check of superset

--- src/homework2/hackerrank/check_strict_superset.py
+++ src/homework2/hackerrank/check_strict_superset.py
@@ -1,4 +1,4 @@
-"""You are given a set  and  other sets.
+"""You are given a set and other sets.
 Your job is to find whether set is a strict superset of the sets.
 A strict superset has at least one element that does not exist in its subset.
 
@@ -8,15 +8,23 @@ The second line contains the number of other sets.
 The next lines contains the space separated elements of the other sets.
 
 Output Format
-Print True if set  is a strict superset of all other  sets. Otherwise,
-print False."""
-a = set([int(x) for x in input().split()])
-n = int(input())
-while n:
-    t = set([int(x) for x in input().split()])
-    if not a.issuperset(t):
+Print True if set is a strict superset of all other sets.
+Otherwise, print False."""
+
+
+def issuperset(superset, subset):
+    if superset - subset and not subset - superset:
+        return True
+    return False
+
+
+superset = set([int(x) for x in input().split()])
+set_number = int(input())
+while set_number:
+    subset = set([int(x) for x in input().split()])
+    if not issuperset(superset, subset):
         print(False)
         break
-    n -= 1
+    set_number -= 1
 else:
     print(True)
