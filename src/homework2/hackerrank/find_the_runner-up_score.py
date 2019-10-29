6ff4347fb479b4cb610944c54087f52259dfaab3
Olga <o.f.akhmetova@gmail.com>
2019-10-29 11:45:14 +0300

    change input processing

--- src/homework2/hackerrank/find_the_runner-up_score.py
+++ src/homework2/hackerrank/find_the_runner-up_score.py
@@ -5,5 +5,5 @@ Output Format
 Print the runner-up score."""
 if __name__ == '__main__':
     n = int(input())
-    arr = map(int, input().split())
-    print(sorted(set(arr))[-2])
+    scores = [int(x) for x in input().split()]
+    print(sorted(set(scores))[-2])
