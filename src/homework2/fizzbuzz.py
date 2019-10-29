1a4953be64732e67f556dff36dbbc709cb6bd583
Olga <o.f.akhmetova@gmail.com>
2019-10-29 11:39:23 +0300

    unification logical operators

--- src/homework2/fizzbuzz.py
+++ src/homework2/fizzbuzz.py
@@ -1,5 +1,5 @@
 for i in range(1, 101):
     if (i % 3 == 0 or i % 5 == 0):
-        print("Fizz" * (not (i % 3)) + "Buzz" * (not (i % 5)))
+        print("Fizz" * (i % 3 == 0) + "Buzz" * (i % 5 == 0))
     else:
         print(i)
