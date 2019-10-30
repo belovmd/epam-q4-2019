fa3ca7264d9aa67310d9c7101b0c9a97742c74d4
par216 <56726139+par216@users.noreply.github.com>
2019-10-30 13:51:50 +0200

    Add files via upload

---
+++ leap_years.py
@@ -0,0 +1,21 @@
+'''
+You are given the year, and you have to write a function to check if the year is leap or not
+'''
+
+
+def LeapYear():
+    year = int(input("Enter year: "))
+
+    if year % 4 != 0:
+        print("non-leap year")
+    else:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                print("leap year")
+            else:
+                print("non-leap year")
+        else:
+            print("leap year")
+
+
+LeapYear()
