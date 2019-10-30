55ade96abc77c5e015a018b9ec0b488651c2bb62
PavelMartysiuk <martys3688@gmail.com>
2019-10-30 09:31:19 +0300

    fix task1

--- src/task1.py
+++ src/task1.py
@@ -1,11 +1,12 @@
 '''Purchase money calculator'''
 
 
-def money_calculator(cost, quantity):
+def money_calculator(dollars,cents,quantity):
+    cost = dollars + cents/100
     dollars = cost * quantity
     cents = int((dollars % 1) * 100)
     return f'Total cost:{int(dollars)} dollars {cents} cents'
 
 
 if __name__ == '__main__':
-    print(money_calculator(3.20, 3))
+    print(money_calculator(3,20, 3))
