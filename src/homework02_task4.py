8625ebdb430b8a0b58ec35821d63854505c65f78
Siarhei Straltsou <siarhei.straltsou@gmail.com>
2019-10-31 18:15:44 +0300

    add: task 4, links to profiles

---
+++ src/homework02_task4.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# coding: utf8
+
+"""
+EPAM python q4 homework 02.4 Task from another resources
+Sergey Streltsov 2019-10-31
+
+Register on https://stepik.org/catalog?language=en,
+https://py.checkio.org/  site (or https://www.codewars.com,
+https://www.hackerrank.com/, https://acmp.ru)
+and solve 1-5 different tasks from Elementary (or any) level.
+Add task description as a module docstring (at the top of the script).
+"""
+
+
+if __name__ == '__main__':
+    python_selenium_lnk = 'https://stepik.org/cert/206988'
+    print('Automate testing process with Selenium and Python: {}'.format(
+        python_selenium_lnk))
+    python_basics_lnk = 'https://stepik.org/cert/25226'
+    print('Python, basics and application: {}'.format(
+        python_basics_lnk))
+    codewars_profile = 'https://www.codewars.com/users/Mantikor'
+    print('My profile ay Codewars, but only JS and Go katas: {}'.format(
+        codewars_profile))
