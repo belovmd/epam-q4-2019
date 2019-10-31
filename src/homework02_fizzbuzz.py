46c7bd35d3a88c39280f78d600d13d9ff8595714
Siarhei Straltsou <siarhei.straltsou@gmail.com>
2019-10-31 18:20:46 +0300

    fix: docstring length

--- src/homework02_fizzbuzz.py
+++ src/homework02_fizzbuzz.py
@@ -6,8 +6,9 @@
 EPAM python q4 homework 02.3 FizzBuzz
 Sergey Streltsov 2019-10-22
 
-Write a program that prints the numbers from 1 to 100, but for multiples
-of three print “Fizz” instead of the number and for multiples of five print “Buzz”.
+Write a program that prints the numbers from 1 to 100,
+but for multiples of three print “Fizz” instead of the number
+and for multiples of five print “Buzz”.
 For numbers which are multiples of both three and five, print “FizzBuzz”.
 ( http://en.wikipedia.org/wiki/Biz... )
 """
