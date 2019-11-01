9f188ae8948acc13fee824d06318004f77589615
par216 <56726139+par216@users.noreply.github.com>
2019-11-01 10:28:07 +0200

    Add files via upload

---
+++ num.py
@@ -0,0 +1,14 @@
+num = int(input("Enter num: "))
+b = ''
+counter = 0
+
+while num:
+    b = str(num % 2) + b
+    num = num // 2
+
+for i in range(len(b)):
+    if b[i] == "1":
+        counter += 1
+
+print(b)
+print(counter)
