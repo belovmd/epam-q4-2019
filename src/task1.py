2a93f090214d8dfde39b96ef8d6ed61dce3e4fbc
PavelMartysiuk <martys3688@gmail.com>
2019-11-01 11:51:46 +0300

    fix format string

--- src/task1.py
+++ src/task1.py
@@ -5,8 +5,7 @@ def money_calculator(dollars, cents, quantity):
     cost = dollars + cents / 100
     dollars = cost * quantity
     cents = int((dollars % 1) * 100)
-    dollars = int(dollars)
-    return 'Total cost:{0} dollars {1} cents'.format(dollars, cents)
+    return 'Total cost: {0} dollars {1} cents'.format(int(dollars), cents)
 
 
 if __name__ == '__main__':
