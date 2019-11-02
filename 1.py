043c7c64c545c9c5783d2bbdf7680c4b25aa48e2
par216 <56726139+par216@users.noreply.github.com>
2019-11-02 16:20:28 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,18 +1,15 @@
 """
 Finding palindrome
 """
-n = int(input("Enter num: "))
+h = int(input("Enter num: "))
 
-if n < 0:
-    print("NO")
-else:
-    k = n
-    m = 0
-    while k > 0:
-        m = 10 * m + k % 10
-        k = k // 10
+k = abs(h)
+m = 0
+while k > 0:
+    m = 10 * m + k % 10
+    k = k // 10
 
-if (m == n):
+if (m == abs(h)):
     print("YES")
 else:
     print("NO")
