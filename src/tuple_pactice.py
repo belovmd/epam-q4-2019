a4df479ab106d06967bed49cd8bbe7266f8fef19
PavelMartysiuk <martys3688@gmail.com>
2019-11-07 11:10:04 +0300

    fix f-strings

--- src/tuple_pactice.py
+++ src/tuple_pactice.py
@@ -3,18 +3,18 @@
 # Create  the list ['a', 'b', 'c'], then create tuple from this list
 list_1 = ['a', 'b', 'c']
 tuple_1 = tuple(list_1)
-print(f'{list_1}\n{tuple_1}')
+print('{0}\n{1}'.format(list_1, tuple_1))
 
 # create  the tuple ('a', 'b', 'c'), then create list from this tuple
 tuple_2 = ('a', 'b', 'c')
 list_2 = list(tuple_2)
-print(f'{tuple_2} \n{list_2}')
+print('{0} \n{1}'.format(tuple_2, list_2))
 
 # a = 'a', b = 2, c = 'gamma' in one line
 a, b, c = 'a', 2, 'gamma'
-print(f'a = {a}, \nb = {b} \nc = {c}')
+print('a = {0}, \nb = {1} \nc = {2}'.format(a, b, c))
 
 # Create a tuple containing just a single element  which in turn contains
 # three elements 'a', 'b', 'v'
 tuple_3 = (tuple_2,)
-print(f'tuple: {tuple_3} \nlength = {len(tuple_3)}')
+print('tuple: {0} \nlength = {1}'.format(tuple_3, len(tuple_3)))
