89fecbe4d3c18f64ffb89c9cf6fea5da6cbcb6e3
PavelMartysiuk <martys3688@gmail.com>
2019-11-12 19:13:46 +0300

    fix enumerate

--- src/list_practice.py
+++ src/list_practice.py
@@ -11,7 +11,7 @@ print(list_2)
 # ['1a', '2a', '3a', '4a']
 list_3_0 = [str(digit) + 'a' for digit in range(1, 5)]
 list_3_1 = [sym + 'a' for sym in '1234']
-list_3_2 = [str(num) + sym for num, sym in enumerate('a' * 5) if num != 0]
+list_3_2 = [str(num) + sym for num, sym in enumerate('a' * 4, 1)]
 print('List_3_0:{0}'.format(list_3_0))
 print('List_3_1:{0}'.format(list_3_1))
 print('List_3_2:{0}'.format(list_3_2))
