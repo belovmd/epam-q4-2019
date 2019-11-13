658edd078cec830aac6d002cafd5b00d99de3de6
par216 <56726139+par216@users.noreply.github.com>
2019-11-13 15:29:32 +0300

    Update H3-3.py

--- H3-3.py
+++ H3-3.py
@@ -1,6 +1,6 @@
-'''
+"""
 Dictionary practice
-'''
+"""
 
 
 # Define a function generate_numbers(number)
@@ -8,14 +8,17 @@ Dictionary practice
 # between 1 and n (both included) and the values are square
 # of keys. n–function argument. Default is 20.
 def generate_numbers(number):
-    return dict((i, i ** 2) for i in range(number))
+    return {(i, i ** 2) for i in range(number)}
 
 
 # Define a function count_characters(count_me_string)
 # which count and return the numbers
 # of each character in a count_me_string argument.
 def count_characters(count_me_string):
-    return {i: count_me_string.count(i) for i in count_me_string}
+    dict = {}
+    for i in count_me_string:
+        dict[i] = dict.get(i, 0) + 1
+    return dict
 
 
 print(generate_numbers(20))
