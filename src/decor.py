4096dcf4f0bbe49830f9c8e06a919c31161e9327
PavelMartysiuk <martys3688@gmail.com>
2019-11-13 16:52:58 +0300

    Homework4

---
+++ src/decor.py
@@ -0,0 +1,35 @@
+""" Создать декоратор, который хранит в себе результы вызовов
+функций и время получения программы за время запуска """
+
+from time import time
+from collections import defaultdict
+
+dict_ = defaultdict(list)
+
+
+def storage(func):
+    def wrapper(*args, **kwargs):
+        start_time = time()
+        res = func(*args, **kwargs)
+        dict_[func.__name__].extend([res, start_time - time()])
+        print(dict_)
+        return res
+
+    return wrapper
+
+
+@storage
+def sum_(*args):
+    return sum(args)
+
+
+@storage
+def hi():
+    return 'Hi'
+
+
+if __name__ == '__main__':
+    print((sum_(1, 2, 3)))
+    print(storage(sum_)(1, 2, 3))
+    print(hi())
+    print(dict_)
---
+++ src/get_ranges.py
@@ -0,0 +1,24 @@
+"""Реализовать функцию get_ranges, которая получает на вхох непустой список
+неповторяющихся целых чисел, отсортированных по возрастанию, которая этот
+список сворачивает"""
+
+def get_ranges(lst):
+    out_str = ''
+    for el in range(len(lst)):
+        try:
+            if lst[el] == lst[el + 1] - 1:
+                if out_str == '' or out_str[-1] == ',':
+                    out_str += str(lst[el]) + '-'
+
+            else:
+                out_str += str(lst[el]) + ','
+        except IndexError:
+            out_str += str(lst[el])
+    return out_str
+
+
+if __name__ == '__main__':
+    print(get_ranges([1, 2, 3, 6, 7, 8, 10, 11]))
+    print(get_ranges([0, 1, 2, 3, 4, 7, 8, 10]))
+    print(get_ranges([4, 7, 10]))
+    print(get_ranges([2, 3, 8, 9]))
