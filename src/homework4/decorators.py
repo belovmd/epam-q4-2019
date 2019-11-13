9660ef3fea61d84aaac20f0b4d1eb8d1017db31a
Olga <o.f.akhmetova@gmail.com>
2019-11-13 20:21:32 +0300

    add another solution

--- src/homework4/decorators.py
+++ src/homework4/decorators.py
@@ -24,3 +24,30 @@ def pow(a, b):
 for i, j in zip(range(10 ** 2), range(10 ** 3)):
     pow(i, j)
 print(result)
+
+
+# another solution, sort of cache
+def store2(f):
+    result2 = {}
+
+    def wrapper(*args, **kwargs):
+        if args in result2:
+            return result2[args].result
+        else:
+            start = time.time()
+            for i in range(1000):  # to make time bigger
+                res = f(*args, **kwargs)
+            stop = time.time()
+            result2[args] = {"result": res % (10 ** 8), "time": (stop - start)}
+            return result2[args]
+
+    return wrapper
+
+
+@store2
+def pow2(a, b):
+    return a ** b
+
+
+for i, j in zip(range(10 ** 2), range(10 ** 3)):
+    print(pow2(i, j))
