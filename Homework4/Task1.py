7070a550256664360cd276c41d5a7606621f1ccd
Alex_Khalupka <alexkha2001@gmail.com>
2019-11-14 21:33:08 +0300

    Homework4

---
+++ Homework4/Task1.py
@@ -0,0 +1,31 @@
+import time
+log = []
+
+
+def timer(f):
+    def wrapper(*args, **kwargs):
+        global log
+        t0 = time.time()
+        res = f(*args, **kwargs)
+        t1 = time.time()
+        log.append({"function_name": f.__name__,
+                    "result": res,
+                    "execution_time": t1-t0,
+                    })
+        return res
+    return wrapper
+
+
+@timer
+def func1(x, y):
+    return x + y
+
+
+@timer
+def func2(x, y):
+    return x - y
+
+
+func1(3, 2)
+func2(3, 2)
+print(log)
---
+++ Homework4/Task2.py
@@ -0,0 +1,20 @@
+def get_ranges(lst):
+    diff, first = lst[0], lst[0]
+    ans = str(lst[0])
+    for i in range(len(lst)):
+        if lst[i] == i + diff:
+            if i == len(lst) - 1:
+                ans += "-" + str(lst[i])
+        else:
+            if first != lst[i-1]:
+                ans += "-" + str(lst[i-1]) + "," + str(lst[i])
+            else:
+                ans += "," + str(lst[i])
+            diff = lst[i] - i
+            first = lst[i]
+    return ans
+
+
+print(get_ranges([0, 1, 2, 3, 4, 7, 8, 10]))
+print(get_ranges([4, 7, 10]))
+print(get_ranges([2, 3, 8, 9]))
