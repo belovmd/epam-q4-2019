31b70eebde9295cb836454a3261e4a214fe7886e
par216 <56726139+par216@users.noreply.github.com>
2019-11-15 16:19:35 +0300

    Update H4-2.py

--- H4-2.py
+++ H4-2.py
@@ -1,6 +1,6 @@
 """
-Реализовать функцию get_ranges 
-которая получает на вход непустой список 
+Реализовать функцию get_ranges
+которая получает на вход непустой список
 неповторяющихся целых чисел,
 отсортированных по возрастанию,
 которая этот список “сворачивает”
@@ -11,13 +11,13 @@ def get_ranges(line):
     new_line = ""
     for i in range(len((line)) - 1):
         if line[i] + 1 != line[i + 1]:
-            new_line += str(line[i]) + "," 
+            new_line += str(line[i]) + ","
         elif line[i] - 1 != line[i - 1]:
-            new_line += str(line[i]) + "-"     
-    new_line += str(line[-1])        
+            new_line += str(line[i]) + "-"
+    new_line += str(line[-1])
     return new_line
 
-  
+
 print(get_ranges([0, 1, 2, 3, 4, 7, 8, 10]))  # "0-4,7-8,10"
 print(get_ranges([4, 7, 10]))  # "4,7,10"
 print(get_ranges([2, 3, 8, 9]))  # "2-3,8-9"
