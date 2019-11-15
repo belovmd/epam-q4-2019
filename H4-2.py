bb3ddcbcfe51101ec3fcac780393d54b67b74954
par216 <56726139+par216@users.noreply.github.com>
2019-11-15 16:18:22 +0300

    Update H4-2.py

--- H4-2.py
+++ H4-2.py
@@ -7,16 +7,17 @@
 """
 
 
-def  get_ranges(line):
+def get_ranges(line):
     new_line = ""
     for i in range(len((line)) - 1):
-        if line[i] + 1 != line[i + 1] :
+        if line[i] + 1 != line[i + 1]:
             new_line += str(line[i]) + "," 
         elif line[i] - 1 != line[i - 1]:
             new_line += str(line[i]) + "-"     
     new_line += str(line[-1])        
     return new_line
 
-print(get_ranges([0, 1, 2, 3, 4, 7, 8, 10])) # "0-4,7-8,10"
-print(get_ranges([4,7,10]) )#// "4,7,10"
-print(get_ranges([2, 3, 8, 9])) #// "2-3,8-9"
+  
+print(get_ranges([0, 1, 2, 3, 4, 7, 8, 10]))  # "0-4,7-8,10"
+print(get_ranges([4, 7, 10]))  # "4,7,10"
+print(get_ranges([2, 3, 8, 9]))  # "2-3,8-9"
