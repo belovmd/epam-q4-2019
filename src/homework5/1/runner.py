088978f1139b4174435106e83d55de2ed7018815
Olga <o.f.akhmetova@gmail.com>
2019-11-16 15:57:55 +0300

    optimize task

--- src/homework5/1/runner.py
+++ src/homework5/1/runner.py
@@ -1,12 +1,9 @@
-import inspect
+from inspect import isfunction
 import pytasks
 
 
 def runner(*args):
     if not args:
-        func_tuples = inspect.getmembers(pytasks, inspect.isfunction)
-        args = [f[-1] for f in func_tuples]  # get function object
-    else:
-        args = [getattr(pytasks, func_str) for func_str in args]
-    for func in args:
-        print(func())
+        args = [attr for attr in dir(pytasks) if not attr.startswith("__")]
+    [print(getattr(pytasks, func)()) for func in args if
+     isfunction(getattr(pytasks, func))]
