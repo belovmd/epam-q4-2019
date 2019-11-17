825683eadda03250dc8fc5aeefb7aa5762da0c5f
nikolay <83adnike@gmail.com>
2019-11-17 15:00:35 +0300

    Homework5: Nikolai Potovin(first part)

--- src/runner.py
+++ src/runner.py
@@ -2,8 +2,10 @@
 def runner(*functions):
     """Функция импорта из pytasks.py"""
     if not functions:
-        from pytasks import generate_numbers, count_characters, \
-                            fizzbuzz, is_palindrome
+        from pytasks import generate_numbers
+        from pytasks import count_characters
+        from pytasks import fizzbuzz
+        from pytasks import is_palindrome
         print(generate_numbers())
         print(count_characters())
         print(fizzbuzz())
