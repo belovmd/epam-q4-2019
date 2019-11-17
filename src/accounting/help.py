fc37620fec7706ead597b1e88f70e37a950dd479
PavelMartysiuk <martys3688@gmail.com>
2019-11-17 19:15:55 +0300

    add description

--- src/accounting/help.py
+++ src/accounting/help.py
@@ -1,4 +1,10 @@
-""" Function for saving and conversion dict to str """
+""" Функции для создания словаря, считывания, сохарнения и преобразования в строку
+ create_outfile - вызывается при запуске программы,
+ создает файл data.json, который хранит пустой словарь
+ rd_dict - считывает файл data.json, вызывается при каждом запуске программы
+ save_dict - перезаписывает файл data.json, вызывается принажатии на кнопку save
+ show_all - преобразовывает словарь в строку,
+ вызывается при нажатии на кнопку show_all"""
 
 import json as js
 from os import getcwd
@@ -25,7 +31,7 @@ def rd_dict():
 
 def save_dict(dict_):
     with open('data.json', 'w') as wr_file:
-        js.dump(dict_, wr_file)
+        js.dump(dict_, wr_file, indent=2)
 
 
 def show_all(dict_):
