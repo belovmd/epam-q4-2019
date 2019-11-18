874bbc13b5b113ae96e8d5e31a0cc6a5bc405b9d
alStrokau <strokov.aleksandr@gmail.com>
2019-11-18 16:46:17 +0300

    fix output format

--- src/task2.py
+++ src/task2.py
@@ -16,6 +16,16 @@ def get_ranges(lst):
         else:
             list_to_add.append([item1, item2])
 
+    def print_ranges(lsts):
+        line = ""
+
+        for lst in lsts:
+            if len(lst) == 2:
+                line = "{},{}-{}".format(line, lst[0], lst[1])
+            else:
+                line = '{},{}'.format(line, lst[0])
+        print(line[1:])
+
     rez = list()
     start = lst[0]
     curr = lst[0]
@@ -28,9 +38,9 @@ def get_ranges(lst):
             start = x
             curr = x
     append_range(rez, start, curr)
-    return rez
+    print_ranges(rez)
 
 
-print(get_ranges([0, 1, 2, 3, 4, 7, 8, 10]))
-print(get_ranges([4, 7, 10]))
-print(get_ranges([2, 3, 8, 9]))
+get_ranges([0, 1, 2, 3, 4, 7, 8, 10])
+get_ranges([4, 7, 10])
+get_ranges([2, 3, 8, 9])
