b70c01eb4ae26135470dc945ab648d247067ad47
Siarhei Straltsou <siarhei.straltsou@gmail.com>
2019-11-20 19:01:31 +0300

    hw07, task 1.2

--- src/homework07.py
+++ src/homework07.py
@@ -7,8 +7,6 @@ EPAM python q4 homework 07
 Sergey Streltsov 2019-11-20
 """
 
-import hashlib
-
 
 def five_divide(divider=0):
     try:
@@ -17,6 +15,16 @@ def five_divide(divider=0):
         print('Error: {}'.format(e))
 
 
+def print_list_element(thelist, index):
+    try:
+        print(thelist[index])
+    except IndexError as e:
+        print('Error: {}'.format(e))
+
+
 if __name__ == '__main__':
     five_divide(divider=5)
     five_divide()
+    print_list_element(thelist=[1, 2, 3, 4, 5, 6], index=0)
+    print_list_element(thelist=[1, 2, 3, 4, 5, 6], index=3)
+    print_list_element(thelist=[1, 2, 3, 4, 5, 6], index=6)
