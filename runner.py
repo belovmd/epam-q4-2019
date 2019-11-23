fecb0243bb1020b0115b8be5cb3615610e253acc
par216 <56726139+par216@users.noreply.github.com>
2019-11-23 18:45:30 +0300

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,7 +1,23 @@
-from pytasks import generate_numbers
+""" Write two modules pytasks.py and runner.py. Module pytasks contains
+definition for functions from previous tasks.  Separate module runner
+contains runner() function. runner module can be imported or started
+from command line as script.
+generate_numbers()
+count_characters()
+fizzbuzz() – returns list with values
+is_palindrome() - returns True or False.
+runner() function can be called as:
+runner() – all functions will be called with default values and result
+printed to screen
+runner(‘generate_numbers’) – print result  only for generate_numbers()
+runner(‘generate_numbers’, ‘count_characters’) – print results for
+generate_numbers() and  count_characters(). Any combination of functions
+can be specified. """
+
 from pytasks import count_characters
 from pytasks import is_palindrome
 from pytasks import fizz_buzz
+from pytasks import generate_numbers
 import pytasks
 
 
