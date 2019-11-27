fd5722cc09a820c592c0815c94fb61fcbef73948
Aleksandr Levdansky <coromilly@gmail.com>
2019-11-28 01:00:41 +0300

    fixed some issues.

--- warehouse_simulator.py
+++ warehouse_simulator.py
@@ -10,155 +10,151 @@ class Warehouse(object):
     """This is simulator of Warehouse.
 
     Starting parameters:
-    material_limit - material limit in m^3
     money - starting sum of money in dollars
     cheat - difference between purchase price and sale price
-    extend_cost - cost in dollars of Warehouse extending at 1m^3
     """
-    material_limit = 5000
     money = 70000
     cheat = 1.1
-    extend_cost = 10
     materials = {}
+    price = {}
 
-    @staticmethod
-    def warehouse_info():
-        """Prints materials at Warehouse and their Quantity/Capacity."""
+    def warehouse_info(self):
+        """Prints materials at Warehouse and their quantity."""
         for material in Warehouse.materials.keys():
-            print('Quantity/Capacity of {mater}: {amount}/{capacity}.'.format(
-                mater=Warehouse.materials[material].material_name,
-                amount=Warehouse.materials[material].material_quantity,
-                capacity=Warehouse.materials[material].material_limit))
-        print('Current number of materials in Warehouse is {}.'.
-              format(len(Warehouse.materials)))
+            print('Quantity of {mater}: {quantity}.'.format(
+                mater=material,
+                quantity=Warehouse.materials[material]))
+        number = 0
+        for value in Warehouse.materials.values():
+            if value != 0:
+                number += 1
+        print('Current number of materials in Warehouse is {}.'
+              'Current money ballance: {}'.
+              format(number, Warehouse.money))
+
+    def material_info(self, material):
+        """Shows current info about material."""
+        print('Material - {}: Price: {}, Quantity: {}'.format(
+            material.material_name,
+            Warehouse.price[material.material_name],
+            Warehouse.materials[material.material_name]))
 
 
-class Material(Warehouse):
-    """Working with materials at Warehouse."""
+class Material(object):
+    """Creating materials and adding their quantity and price to Warehouse."""
+    price = {}
+
     def __init__(self, material_name, material_price):
         """Add material to Warehouse."""
         self.material_name = material_name
         self.material_price = material_price
-        self.material_quantity = Warehouse.material_limit
-        self.material_limit = Warehouse.material_limit
-        Warehouse.materials[self.material_name] = self
+        self.material_quantity = 0
+        Warehouse.materials[self.material_name] = self.material_quantity
+        Warehouse.price[self.material_name] = self.material_price
 
-    def material_purchase(self, purchase_quantity):
+
+class Worker(Warehouse):
+    """Creating worker, who can buy/sell materials, extend capacity of
+
+    Warehouse and check about material price raise.
+    """
+
+    def material_purchase(self, material, quantity):
         """Material purchase."""
-        if purchase_quantity * self.material_price < Warehouse.money:
-            if self.material_quantity + purchase_quantity <= \
-                    self.material_limit:
-                self.material_quantity += purchase_quantity
-                print('You purchased {}m^3 of {}. Current quantity is {}/{}. '
-                      'You spent {}$.'.format(purchase_quantity,
-                                              self.material_name,
-                                              self.material_quantity,
-                                              self.material_limit,
-                                              purchase_quantity *
-                                              self.material_price))
-            else:
-                print('There will be too much material to store '
-                      'at warehouse. Sale some or extend capacity.')
+        if quantity * material.material_price <= Warehouse.money:
+            Warehouse.materials[material.material_name] += quantity
+            Warehouse.money -= quantity * material.material_price
+            print('You purchased {}m^3 of {}. Current quantity is {}. '
+                  'You spent {}$.'.format(quantity,
+                                          material.material_name,
+                                          Warehouse.
+                                          materials[material.material_name],
+                                          quantity *
+                                          material.material_price))
         else:
             print('There is not enough money to purchase. Sale some.')
 
-    def material_sale(self, sale_quantity):
+    def material_sale(self, material, quantity):
         """Material sale."""
-        if self.material_quantity > sale_quantity:
-            self.material_quantity -= sale_quantity
-            money = sale_quantity * self.material_price * Warehouse.cheat
+        if Warehouse.materials[material.material_name] >= quantity:
+            Warehouse.materials[material.material_name] -= quantity
+            money = quantity * material.material_price * Warehouse.cheat
             Warehouse.money += money
-            print('You sale {}m^3 of {}. Current quantity is {}/{}. '
-                  'You earn {}$.'.format(sale_quantity,
-                                         self.material_name,
-                                         self.material_quantity,
-                                         self.material_limit,
+            print('You sale {}m^3 of {}. Current quantity is {}. '
+                  'You earn {}$.'.format(quantity,
+                                         material.material_name,
+                                         Warehouse.
+                                         materials[material.material_name],
                                          money))
         else:
             print('There is not enough material to sale. Purchase some.')
 
-    def extend_capacity(self, extend_value):
-        if extend_value * Warehouse.extend_cost <= Warehouse.money:
-            self.material_limit += extend_value
-            Warehouse.money -= extend_value * Warehouse.extend_cost
-            print('Capacity extended. Current = {capacity}. '
-                  'Spent {money}$.'.format(capacity=self.material_limit,
-                                           money=extend_value *
-                                           Warehouse.extend_cost))
-        else:
-            print('Need to sell materials at {sum}$ to extend capacity.'.
-                  format(sum=extend_value * Warehouse.extend_cost -
-                         Warehouse.money))
-
-    def raise_price(self, amount_percent):
+    def raise_price(self, material, amount_percent):
         """Checking price raise.
 
         If price raise more than 5%, we don't buy material.
         """
-        if amount_percent > 0.05 * self.material_price:
+        if amount_percent > 0.05 * material.material_price:
             print('Price increase too much: {}.'.format(
-                self.material_price + amount_percent / 100 *
-                self.material_price))
+                material.material_price + amount_percent / 100 *
+                material.material_price))
         else:
-            self.material_price += amount_percent / 100 * self.material_price
-
-    def info(self):
-        """Shows current info about material."""
-        print('Material - {}: Price: {}, In stock: {}/{}'.format(
-            self.material_name,
-            self.material_price,
-            self.material_quantity,
-            self.material_limit))
+            material.material_price += \
+                amount_percent / 100 * material.material_price
+            Warehouse.price[material.material_name] += \
+                amount_percent / 100 * material.material_price
 
 
-class Calculator(Material):
+class Calculator(Warehouse):
     """Counts the purchase amount of several materials."""
-    @staticmethod
-    def calc():
+
+    def calculate(self):
         """Helps to count your purchases and makes "check.txt"."""
         purchases = 'Your purchases:\n'
         summary = 0
         add_to_cart = {}
         for material in Warehouse.materials.keys():
-            amount = int(input('Input amount of {}: '.format(
+            amount = float(input('Input amount of {}: '.format(
                 material)))
-            while amount > Warehouse.materials[material].material_quantity:
+            while amount > Warehouse.materials[material]:
                 print('Sorry, max is {}'.format(
-                    Warehouse.materials[material].material_quantity))
-                amount = int(input('Input amount of {}: '.format(
+                    Warehouse.materials[material]))
+                amount = float(input('Input amount of {}: '.format(
                     material)))
             add_to_cart[material] = amount
-            summary += Warehouse.materials[material].material_price * amount
-            purchases += '{amnt} m^3 of {mater} will cost {price}.\n'.format(
+            summary += Warehouse.price[material] * amount
+            purchases += '{amnt} m^3 of {mater} will cost {price}$.\n'.format(
                 amnt=amount,
                 mater=material,
-                price=Warehouse.materials[material].material_price * amount)
+                price=Warehouse.price[material] * amount)
         with open('check.txt', 'w') as check:
-            check.write(purchases + '\n' + 'Total cost: ' + str(summary))
+            check.write(purchases + '\n' + 'Total cost: '
+                        + str(summary) + '$')
         print('\n' + purchases)
-        print('Total cost: ', summary)
+        print('Total cost: ', summary, '$')
 
 
-Sand = Material('Sand', 15)
-Glass = Material('Glass', 20)
-Plastic = Material('Plastic', 5)
-Stone = Material('Stone', 10)
-Metal = Material('Metal', 50)
-Water = Material('Water', 20)
-c = Calculator
-Fuel = Material('Fuel', 70)
-Metal.extend_capacity(1500)
-Warehouse.warehouse_info()
-Water.material_purchase(800)
-Water.extend_capacity(1000)
-Water.info()
-Water.material_purchase(800)
-Water.info()
-Water.material_purchase(1750)
-Water.material_sale(3000)
-Water.material_purchase(1750)
-Fuel.info()
-Fuel.raise_price(7)
-Fuel.raise_price(2)
-Fuel.info()
-c.calc()
+if __name__ == '__main__':
+    sand = Material('Sand', 15)
+    glass = Material('Glass', 20)
+    plastic = Material('Plastic', 5)
+    stone = Material('Stone', 10)
+    metal = Material('Metal', 50)
+    water = Material('Water', 20)
+    fuel = Material('Fuel', 70)
+    warehouse = Warehouse()
+    calc = Calculator()
+    worker = Worker()
+    worker.material_purchase(sand, 1000)
+    worker.material_purchase(glass, 300)
+    worker.material_purchase(plastic, 300)
+    worker.material_purchase(stone, 300)
+    worker.material_purchase(fuel, 300)
+    warehouse.warehouse_info()
+    warehouse.material_info(plastic)
+    worker.material_sale(sand, 500)
+    worker.material_sale(sand, 200)
+    worker.raise_price(fuel, 3)
+    warehouse.material_info(fuel)
+    worker.material_purchase(fuel, 700)
+    calc.calculate()
