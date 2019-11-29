8f5148caa404a4a7f8bdf70e68b823f123d0e2d4
Andrey Kapitonov <49807095+akapitonov1999@users.noreply.github.com>
2019-11-29 16:03:55 +0300

    Update Home4.py

--- Home4.py
+++ Home4.py
@@ -1,20 +1,17 @@
 import math
 import time
 
-result = []
-
 
 # task 1
 def wrapper(f):
     def res(*args, **kwargs):
         t0 = time.time()
         res = f(*args, **kwargs)
-        print(res)
         t1 = time.time()
-        delta_time = t1 - t0
         result.append(res)
-        result.append(delta_time)
-        print(result)
+        delta_time = t1 - t0
+        time_result.append(delta_time)
+        return result, time_result
 
     return res
 
@@ -27,18 +24,19 @@ def func(x, y):
 # task 2
 def get_ranges(int_list):
     output = []
-    i0 = 0
-    i1 = 0
-    while i0 != len(int_list):
-        while i1 < len(int_list) - 1 and int_list[i1] + 1 == int_list[i1 + 1]:
-            i1 += 1
-        if i0 == i1:
-            output.append(str(int_list[i1]))
+    start_position = 0
+    current_position = 0
+    while start_position != len(int_list):
+        while current_position < len(int_list) - 1 \
+                and int_list[current_position] + 1 == int_list[current_position + 1]:
+            current_position += 1
+        if start_position == current_position:
+            output.append(str(int_list[current_position]))
         else:
-            output.append(str(int_list[i0]) + "-" + "(" * (int_list[i1] < 0) +
-                          str(int_list[i1]) + ")" * (int_list[i1] < 0))
-        i1 += 1
-        i0 = i1
+            output.append(str(int_list[start_position]) + "-" +
+                          str(int_list[current_position]))
+        current_position += 1
+        start_position = current_position
     print(output)
 
 
@@ -89,7 +87,7 @@ def angle_inaccuracy(a, b, c, inaccuracy):
     v2 = (a ** 2 + b ** 2 - c ** 2) ** 2 / (4 * b ** 4 * c ** 2)
     v3 = (a ** 2 - b ** 2 + c ** 2) ** 2 / (4 * b ** 2 * c ** 4)
     v4 = (4 * b ** 2 * c ** 2 - (b ** 2 + c ** 2 - a ** 2) ** 2) \
-        / (4 * b ** 2 * c ** 2)
+         / (4 * b ** 2 * c ** 2)
     formula = (v1 + v2 + v3) / v4
     delta_alpha = math.sqrt(formula) * inaccuracy
     r_file = math.degrees(delta_alpha)
