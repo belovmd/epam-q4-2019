0243acfc0507aee14925750b1db2ef647d271427
stychevsky <stychnevsky@gmail.com>
2019-12-01 00:19:22 +0300

    refactored and improved
    - now cold product can be put only in cold camera
    - take_product method do not have refr argument now
    - create dicts instead of two similar variables
    - master will level up after fixing all types of refrigerators
    - limit of masters skill created
    - model rows of every refrigerators class has been created
    - every model can be updated (like new version in real life)
    - refactor program to avoid elifs
    - refactor to key value args in print() functions

--- HW6.py
+++ HW6.py
@@ -1,3 +1,5 @@
+from abc import ABC
+from abc import abstractmethod
 import random
 """Homework 6 - Kitchen
 
@@ -12,60 +14,55 @@ class User(object):
     def __init__(self, name):
         self.name = name
 
-    def take_product(self, refr, product):
-        """Method try to take product from refrigerator
+    def take_product(self, product):
+        """Method try to take product
 
+        Method automatically understand, in which refrigerator product is.
         If refrigerator is broken or product not in it, we do nothing.
-        Otherwise user take product, and cells we it lie become free.
+        Otherwise user take product, and cells where it lie become free.
         """
-        if refr.broken:
-            print('{} is broken!'.format(refr.name))
+
+        refr = product.location
+        if not refr:
+            print('{product_} is in kitchen, not in refrigerator'.format(product_=product.name))
+        elif refr.broken:
+            print('{refr_} is broken!'.format(refr_=refr))
         else:
-            if product.in_freeze_cam == refr:
-                refr.freeze_cells -= product.size
-                product.in_freeze_cam = None
-            elif product.in_cold_cam == refr:
-                refr.cold_cells -= product.size
-                product.in_cold_cam = None
-            else:
-                print('{0} is not in {1}.{2} still hungry'.format(product.name,
-                                                                  refr.name,
-                                                                  self.name))
+            print('{user_} take {product_} from {refr_}'.format(user_=self.name,
+                                                                product_=product.name,
+                                                                refr_=refr.name,
+                                                                ))
+            refr.occupied_cells[product.pr_type] -= product.size
+            product.location = None
 
     def put_product(self, refr, product):
         """Method try to put product in refrigerator
 
-        If refrigerator is broken or product is already in refrigerator, we do
-        nothing.
-
-        Otherwise if it is "cold", we tried to find place for it in cold camera
-        If it it's not enough place in cold camera or it is freeze product,
-        we tried to put product in freeze camera.
-        If it it's not enough place in freeze camera, we will not put product
-        in refrigerator.
+        In cases:
+            1) refrigerator is broken
+            2) product is already in refrigerator
+            3) it it's not enough place in appropriate camera
+        we will not put product in refrigerator.
         """
         if refr.broken:
-            print('{} is broken!'.format(refr.name))
-        elif product.in_cold_cam == refr or product.in_freeze_cam == refr:
-            print('{} is already in {}!'.format(product.name, refr.name))
+            print('{refr_} is broken!'.format(refr_=refr.name))
+        elif product.location == refr:
+            print('{product_} is already in {refr_}!'.format(product_=product.name, refr_=refr.name))
         else:
-            if product.pr_type == 'cold' and (refr.cold_cells + product.size <=
-                                              refr.cold_cam_capacity):
-                product.in_cold_cam = refr
-                refr.cold_cells += product.size
-            elif refr.freeze_cells + product.size <= refr.freeze_cam_capacity:
-                product.in_freeze_cam = refr
-                refr.freeze_cells += product.size
+            camera_type = product.pr_type
+            if refr.occupied_cells[camera_type] + product.size <= refr.capacity[camera_type]:
+                product.location = refr
             else:
-                print('{0} can\'t put {1} in {2} - not enough place!'
-                      .format(self.name, product.name, refr.name))
+                print('{user_name} can\'t put {product_} in {refr_} - not enough place!'.format(user_name=self.name,
+                                                                                                product_=product.name,
+                                                                                                refr_=refr.name))
 
     def eat(self, product):
-        """If product is in refrigerator, in will be deleted forever"""
-        if not product.in_freeze_cam or not product.in_cold_cam:
-            print('{} can\'t eat product from refrigerator!'.format(self.name))
+        """User can eat product if is not in refrigerator, and product object will be deleted forever"""
+        if not product.location:
+            print('{user_} can\'t eat product from refrigerator!'.format(user_=self.name))
         else:
-            print('{} eat {}'.format(self.name, product.name))
+            print('{user_} eat {product_}'.format(user_=self.name, product_=product.name))
             del product
 
 
@@ -76,12 +73,12 @@ class Parent(User):
     def call_to_master(self, master, refr):
         """Using this method parent can call to master.
 
-        After call master will try to fix refrigerator using master.try_to_fix
+       After call master will try to fix refrigerator using master.try_to_fix
         method. This method returns result (True o False), and this result will
         be printed
         """
         job_result = master.try_to_fix(self, refr)
-        print('{} called to {}.'.format(self.name, master.name), end=' ')
+        print('{user_} called to {master_}.'.format(user_=self.name, master_=master.name), end=' ')
         print('He fixed it' if job_result else 'He can\'t fix it')
 
     def earn_money(self, salary=200):
@@ -95,14 +92,23 @@ class Child(User):
         """After hit refriger. will be broken with a thirty percent chance """
         if random.random() < 0.3:
             refr.broken = True
-            print('{} broke {}!'.format(self.name, refr.name))
+            print('{child_name} broke {refr_}!'.format(child_name=self.name, refr_=refr.name))
         else:
-            print('{} hit {}, but not broke.'.format(self.name, refr.name))
+            print('{child_name} hit {refr_}, but not broke.'.format(child_name=self.name, refr_=refr.name))
 
 
 class Master(object):
-    """Master is a person who can fix refrigerators if he have enough skill"""
+    """Master is a person who can fix refrigerators if he have enough skill
+
+    P.S. I understand that coeefs, fix_difficulty and skills_profit can be properties of Refrigerator class and it
+    better for OOP conception. But it will make my program more difficult for understanding. So, that's why I create
+    this dicts in Master class."""
     money = 100
+    max_skill = 20
+    study_cost = 100
+    coeefs = {'The Netherlands': 1.5, 'Belarus': 1}
+    fix_difficulty = {'The Netherlands': 1.5, 'Belarus': 1}
+    skills_profit = {'The Netherlands': 0.2, 'Belarus': 0.5}
 
     def __init__(self, skill, name='Master', company='own'):
         self.skill = skill
@@ -111,54 +117,47 @@ class Master(object):
 
     def advertisement(self):
         """Advertising of master. Skills always increase by two"""
-        print('Highly skilled master {0} from {1} company is looking for work!'
-              ' My skill is {2}. Prices:\nImport refrigerator: {3}\nDomestic '
-              'refrigerator: {4}'.format(self.name, self.company,
-                                         str(self.skill + 2),
-                                         str(self.skill * 150),
-                                         str(self.skill * 100)))
+        print('Highly skilled master {master_name} from {company_name} company is looking for work! My skill is '
+              '{master_skill}. Prices:\nImport refrigerator: {price_import_refr}\n Domestic refrigerator: '
+              '{price_domestic_refr}'.format(master_name=self.name,
+                                             company_name=self.company,
+                                             master_skill=str(self.skill + 2),
+                                             price_import_refr=str(self.skill * 100 * self.coeefs['The Netherlands']),
+                                             price_domestic_refr=str(self.skill * 100 * self.coeefs['Belarus'])
+                                             ))
 
     def try_to_fix(self, customer, refr):
         """Master will try to fix refrigerator
 
         Price formula: skill * 100 * сoeff
-        Netherlands refrigerators can be fixed by only masters with >=7 skill,
-        coeff is 1,5
-        Belarus refrigerators can be fixed only by masters with >=3 skill,
-        coeff is 1, and also help to improve some skills.
+        Master can fix refrigerator only if he has enough skill. If not - consultation is free.
+        After fixing master improve skills and earn money.
         Payment is made by the client (Parent)
-        Сonsultation is free.
         """
-        if refr.made_in == 'The Netherlands':
-            if self.skill >= 7:
-                refr.broken = False
-                customer.money -= self.skill * 100 * 1.5
-                return True
-        elif refr.made_in == 'Belarus':
-            if self.skill >= 3:
-                refr.broken = False
-                customer.money -= self.skill * 100 * 1
-                self.skill += 0.5
-                return True
+
+        if self.skill >= self.fix_difficulty[refr.made_in]:
+            refr.broken = False
+            customer.money -= self.skill * 100 * self.coeefs[refr.made_in]
+            self.skill += self.skills_profit[refr.made_in]
+            return True
         return False
 
     def study(self):
         """Master can pay money and improve his skills"""
-        if self.money >= 100:
-            self.money -= 100
+        if self.money >= self.study_cost:
+            self.money -= self.study_cost
             self.skill += 1
         else:
-            print('{self.master} don\'t have enough money to study')
+            print('{master_name} don\'t have enough money to study'.format(master_name=self.name))
 
 
 class Product(object):
     """Product is some food we can eat or put to refrigerator
 
     It can be cold and freeze type.
-    Every product aso have size -oit shows how many cells this product need
+    Every product aso have size - it shows how many cells this product need
     """
-    in_cold_cam = None
-    in_freeze_cam = None
+    location = None
 
     def __init__(self, name, pr_type='cold', size=1):
         self.name = name
@@ -166,48 +165,63 @@ class Product(object):
         self.size = size
 
 
-class Refrigerator(object):
+class Refrigerator(ABC):
     """Refrigerator is place where we can save products
 
     New refrigerator always is empty and able to work
     """
-    freeze_cells = 0
-    cold_cells = 0
+    occupied_cells = {'freeze': 0, 'cold': 0}
     broken = False
 
-    def __init__(self, name):
+    @property
+    @abstractmethod
+    def models_capacity(self):
+        return self.models_capacity
+
+    def __init__(self, name, model):
         self.name = name
+        self.capacity = self.models_capacity[model]
+
+    @classmethod
+    def create_or_update_model(cls, model, freeze_cam_capacity, cold_cam_capacity):
+        """Change models characteristics, or create if it is new model"""
+        cls.models_capacity[model] = {'freeze': freeze_cam_capacity, 'cold': cold_cam_capacity}
 
     def display_info(self):
         """Show information about refrigerator and free cells"""
-        print('Hi, I am {0} aka {1}\nNow it is {2} free cold cells'
-              ' and {3} free freeze cells '.format(type(self).__name__,
-                                                   self.name,
-                                                   self.cold_cells,
-                                                   self.freeze_cells))
+        print('It is {refr_type} aka {refr_name}\nNow it is {free_cold_cells} free cold cells and {free_freeze_cells} '
+              'free freeze cells '.format(refr_type=type(self).__name__,
+                                          refr_name=self.name,
+                                          free_cold_cells=self.capacity['cold'] - self.occupied_cells['cold'],
+                                          free_freeze_cells=self.capacity['freeze'] - self.occupied_cells['freeze']))
 
 
 class RefrigeratorAtlant(Refrigerator):
     """Belorussian refrigerator with small capacity"""
     made_in = 'Belarus'
-    freeze_cam_capacity = 4
-    cold_cam_capacity = 6
+    models_capacity = {'Minsk': {'freeze': 4, 'cold': 6},
+                       'Lider': {'freeze': 3, 'cold': 2}}
 
 
 class RefrigeratorPhillips(Refrigerator):
     """High quality netherlands refrigerator with meddle capacity"""
     made_in = 'The Netherlands'
-    freeze_camera_capacity = 6
-    cold_camera_capacity = 12
+    models_capacity = {'model A': {'freeze': 6, 'cold': 12},
+                       'model B': {'freeze': 7, 'cold': 14},
+                       'model C': {'freeze': 20, 'cold': 0}}
 
 
 def example():
-    refr1 = RefrigeratorAtlant('Atlant M150')
+    RefrigeratorAtlant.create_or_update_model('Minsk', 5, 6)
+    refr1 = RefrigeratorAtlant(name='Atlant M150', model='Minsk')
+    refr1.display_info()
     mam = Parent('Olga')
     child1 = Child('Vitya')
     while not refr1.broken:
         child1.hit_fridge(refr1)
     master1 = Master(7)
+    master1.study()
+    master1.study()
     master1.advertisement()
     if refr1.broken:
         mam.call_to_master(master1, refr1)
@@ -216,9 +230,9 @@ def example():
     mam.put_product(refr1, apple)
     mam.put_product(refr1, meat)
     refr1.display_info()
-    mam.take_product(refr1, meat)
+    mam.take_product(meat)
     refr1.display_info()
-    mam.take_product(refr1, meat)
+    mam.take_product(meat)
 
 
 example()
